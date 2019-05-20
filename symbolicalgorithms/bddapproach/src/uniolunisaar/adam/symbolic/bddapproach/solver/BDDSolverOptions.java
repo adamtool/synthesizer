87103b3960a74f67c2808cdfbb30d3e142821237
Manuel Gieseking <mgieseking@users.noreply.github.com>
2019-05-20 20:49:48 +0200

    MG: added the abbility to also add quabs and the bdd libraries to the properties file.

--- symbolicalgorithms/bddapproach/src/uniolunisaar/adam/symbolic/bddapproach/solver/BDDSolverOptions.java
+++ symbolicalgorithms/bddapproach/src/uniolunisaar/adam/symbolic/bddapproach/solver/BDDSolverOptions.java
@@ -1,6 +1,15 @@
 package uniolunisaar.adam.symbolic.bddapproach.solver;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
 import uniolunisaar.adam.ds.solver.SolverOptions;
+import uniolunisaar.adam.exceptions.symbolic.bddapproach.NoSuchBDDLibraryException;
+import uniolunisaar.adam.tools.AdamProperties;
+import uniolunisaar.adam.tools.Logger;
 
 /**
  *
@@ -23,22 +32,55 @@ public class BDDSolverOptions extends SolverOptions {
         super("bdd");
     }
 
-    public BDDSolverOptions(String name, String libraryName, int maxIncrease, int initNodeNb, int cacheSize) {
-        super(name);
-        this.libraryName = libraryName;
-        this.maxIncrease = maxIncrease;
-        this.initNodeNb = initNodeNb;
-        this.cacheSize = cacheSize;
-    }
-
-    public BDDSolverOptions(boolean gg, boolean ggs, boolean pgs) {
-        super("bdd");
-        this.gg = gg;
-        this.ggs = ggs;
-        this.pgs = pgs;
-    }
-
-    public void setLibraryName(String libraryName) {
+//    public BDDSolverOptions(String name, String libraryName, int maxIncrease, int initNodeNb, int cacheSize) {
+//        super(name);
+//        this.libraryName = libraryName;
+//        this.maxIncrease = maxIncrease;
+//        this.initNodeNb = initNodeNb;
+//        this.cacheSize = cacheSize;
+//    }
+//
+//    public BDDSolverOptions(boolean gg, boolean ggs, boolean pgs) {
+//        super("bdd");
+//        this.gg = gg;
+//        this.ggs = ggs;
+//        this.pgs = pgs;
+//    }
+    public void setLibraryName(String libraryName) throws NoSuchBDDLibraryException {
+        if (!(libraryName.equals("buddy") || libraryName.equals("cudd") || libraryName.equals("cal") || libraryName.equals("jdd") || libraryName.equals("java"))) {
+            throw new NoSuchBDDLibraryException(libraryName);
+        }
+        if (!libraryName.equals("java")) {
+            String libsPath = System.getProperty("java.library.path");
+            String[] libs = libsPath.split(File.pathSeparator);
+            boolean found = false;
+            for (String lib : libs) {
+                if (lib.equals(libraryName)) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                String libPath = AdamProperties.getInstance().getProperty(libraryName);
+                System.setProperty("java.library.path", libsPath + File.pathSeparator + libPath);
+                // todo: the next lines are a hack since java does not allow to 
+                // change the java.library.path after the starting of the JVM
+                // compare: https://stackoverflow.com/questions/5419039/is-djava-library-path-equivalent-to-system-setpropertyjava-library-path/24988095#24988095
+                // another possibillity is to set it with -D as VM parameter or
+                // to adapt the javabdd library to allow in the BuddyFactory and
+                // all the other classes for having an external link to the library
+                // The only way of getting rid of the warning seems to be :
+                // System.err.close(); // maybe this is not the best way...
+                // We cannot redirect, because they have saved the System.err in
+                // an early state for putting their warning.
+                try {
+                    Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+                    fieldSysPath.setAccessible(true);
+                    fieldSysPath.set(null, null);
+                } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException ex) {
+                    throw new NoSuchBDDLibraryException(libraryName + " (add to path error)");
+                }
+            }
+        }
         this.libraryName = libraryName;
     }
 
