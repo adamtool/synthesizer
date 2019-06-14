5c10f1443169c28cc542f034c5ee4a93f7371cee
Manuel Gieseking <mgieseking@users.noreply.github.com>
2019-06-14 15:58:47 +0200

    MG: adapted the webinterface.

--- symbolicalgorithms/bddapproach/src/uniolunisaar/adam/symbolic/bddapproach/solver/BDDASafetySolver.java
+++ symbolicalgorithms/bddapproach/src/uniolunisaar/adam/symbolic/bddapproach/solver/BDDASafetySolver.java
@@ -183,7 +183,7 @@ public class BDDASafetySolver extends BDDSolver<Safety> implements BDDType2Solve
      *
      * @return BDD for all system2 transitions for a concurrency preserving net.
      */
-    private BDD sys2TransitionCP(Transition t) {        
+    private BDD sys2TransitionCP(Transition t) {
         Set<Place> pre = t.getPreset();
         BDD sys2 = firable(t, false, 0);
         for (int i = 1; i < getSolvingObject().getMaxTokenCount(); ++i) {
@@ -697,7 +697,9 @@ public class BDDASafetySolver extends BDDSolver<Safety> implements BDDType2Solve
             // pi=pi'
             sysT.andWith(placesEqual(i));
             // \not topi=>ti=ti'
-            BDD impl = TOP[0][i - 1].ithVar(0).impWith(commitmentsEqual(i).andWith(TYPE[0][i - 1].buildEquals(TYPE[1][i - 1])));
+            BDD impl = TOP[0][i - 1].ithVar(0).impWith(
+                    commitmentsEqual(i).andWith(TYPE[0][i - 1].buildEquals(TYPE[1][i - 1]))
+            );
             sysT.andWith(impl);
         }
         return sysT;
